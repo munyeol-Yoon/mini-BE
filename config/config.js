7ed21843ff96aaea984d11ffb22441b952a1dace
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-15 16:42:24 +0900

    update : config.js 파일 production, test 수정

--- config/config.js
+++ config/config.js
@@ -8,17 +8,17 @@ require("dotenv").config();
   dialect: "mysql",
 }),
   (test = {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
     dialect: "mysql",
   }),
   (production = {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
     dialect: "mysql",
   });
 
