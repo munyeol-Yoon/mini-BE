b23eb53e8e86b737dd7b5e98f90f5df46af548e7
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-15 17:57:12 +0900

    bug : plz

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -1,14 +1,5 @@
 EPOSITORY=/home/ubuntu
 cd $REPOSITORY
 
-#!/bin/bash
-
-# I want to make sure that the directory is clean and has nothing left over from
-# previous deployments. The servers auto scale so the directory may or may not
-# exist.
-if [ -d /home/ubuntu/mini-BE ]; then
-    rm -rf /home/ubuntu/mini-BE/
-fi
-mkdir -vp /home/ubuntu/mini-BE/
 
 pm2 delete all
