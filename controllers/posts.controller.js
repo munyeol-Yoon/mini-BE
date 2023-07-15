3245d20abe7271f4135a0f314dabfd8a38098098
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-15 16:37:27 +0900

    update : post api catch 문 출력 수정

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,14 +1,13 @@
-const { Posts } = require('../models');
+const { Posts } = require("../models");
 
 const createPost = async (req, res) => {
   try {
-
     const { title, content, name } = req.body;
     const savedPost = await Posts.create({ title, content, name });
     res.status(201).json({ message: "게시글을 생성하였습니다." });
   } catch (error) {
     console.log(error);
-    res.status(400).json({ errormessage: "게시글 생성에 실패하였습니다." });
+    res.status(400).json({ errorMessage: error.message });
   }
 };
 
@@ -18,9 +17,9 @@ const findAllPosts = async (req, res) => {
     res.json({ data });
   } catch (error) {
     console.log(error);
-    res.status(400).json({ errormessage: "게시글 조회에 실패하였습니다." });
-  }};
-
+    res.status(400).json({ errorMessage: error.message });
+  }
+};
 
 const findPost = async (req, res) => {
   return res.send("게시글 상세 조회");
