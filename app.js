185bd2a011dd0b9b18c04abf5260ec577d6bd3d5
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-15 16:14:41 +0900

    bug : asd

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.send("Home 입니다. 제발");
+  return res.send("Home 입니다. 제발aa");
 });
 
 app.use("/api", indexRouter);
