9f6b4741b6b1bcac75c8256ce60987fabaa013ba
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-15 18:17:54 +0900

    update : CORS 처리 추가

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ require("dotenv").config();
 
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let corsOptions = {
+  origin: "*",
+  credential: true,
+};
+
 const indexRouter = require("./routes");
 
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 
