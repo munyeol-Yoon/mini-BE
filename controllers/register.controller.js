1f6e3e93123897a46e9e0f192b00ad7ae6a0f7e0
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-18 03:10:15 +0900

    update : 로그인 성공 메시지 수정2

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -55,7 +55,9 @@ const loginController = async (req, res) => {
     });
     res.cookie("userToken", `Bearer ${token}`, { httpOnly: true });
 
-    return res.status(200).json({ message: "로그인에 성공하였습니다. " });
+    return res
+      .status(200)
+      .json({ data: { token: token, message: "로그인에 성공하였습니다." } });
   } catch (error) {
     console.log(error);
     if (error.isJoi) {
