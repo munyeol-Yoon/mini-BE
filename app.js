6ee6f88d30d1b2b5e20718f5039879636ee940af
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-18 06:48:15 +0900

    fix : 게시글 업데이트시 수정 API 가 제대로 작동안하는 문제

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", indexRouter);
 
