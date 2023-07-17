8eb565ee9d5ee07773b08e0892fab1445c1ddcd1
GabrielaJeong <fbrkqls94@gmail.com>
2023-07-17 20:10:28 +0900

    fix: fixed logic updated

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,19 +1,86 @@
-const { Posts } = require("../models");
+const { Posts } = require('../models');
+const Joi = require('joi');
+
+const postSchema = Joi.object({
+  title: Joi.string()
+    .trim()
+    .empty('')
+    .max(50)
+    .required()
+    .messages({
+      'any.required': '제목을 입력해주세요.',
+      'string.max': `'제목'은 {#limit} 글자 이하로 입력해주세요.`,
+    }),
+  content: Joi.string()
+    .trim()
+    .empty('')
+    .max(5000)
+    .required()
+    .messages({
+      'any.required': '내용을 입력해주세요.',
+      'string.max': `'내용'은 {#limit} 글자 이하로 입력해주세요.`,
+    }),
+  name: Joi.string()
+    .trim()
+    .empty('')
+    .max(10)
+    .required()
+    .messages({
+      'any.required': '닉네임을 입력해주세요.',
+      'string.max': `'닉네임'은 {#limit} 글자 이하로 입력해주세요.`,
+    }),
+  imgsrc: Joi.string()
+    .uri()
+    .allow(null)
+    .optional()
+});
+
+const updatePostschema = Joi.object({
+  title: Joi.string()
+    .trim()
+    .empty('')
+    .max(50)
+    .required()
+    .messages({
+      'any.required': '제목을 입력해주세요.',
+      'string.max': `'제목'은 {#limit} 글자 이하로 입력해주세요.`,
+    }),
+  content: Joi.string()
+    .trim()
+    .empty('')
+    .max(5000)
+    .required()
+    .messages({
+      'any.required': '내용을 입력해주세요.',
+      'string.max': `'내용'은 {#limit} 글자 이하로 입력해주세요.`,
+    }),
+  imgsrc: Joi.string()
+    .uri()
+    .allow(null)
+    .optional()
+});
+
+
 
 const createPost = async (req, res) => {
+  const { error } = postSchema.validate(req.body); // 본문 스키마 충족조건 판별
+  if (error) {
+    return res.status(400).json({ errorMessage: error.details[0].message });
+  }
+
   try {
-    const { title, content, name } = req.body;
-    const savedPost = await Posts.create({ title, content, name });
+    const { title, content, name, imgsrc } = req.body;
+    const savedPost = await Posts.create({ title, content, name, imgsrc });
     return res.status(201).json({ message: "게시글을 생성하였습니다." });
   } catch (error) {
     console.log(error);
     return res
       .status(400)
-      .json({ errorMessage: "게시글 생성에 실패하였습니다." });
+      .json({ errorMessage: "게시글 생성에 실패하였습니다. " });
   }
 };
 
-const findAllPosts = async (_, res) => {
+const findAllPosts = async (req, res) => {
   try {
     const data = await Posts.findAll({ order: [["createdAt", "DESC"]] });
     return res.status(200).json({ data });
@@ -46,9 +113,13 @@ const findPost = async (req, res) => {
 };
 
 const updatePost = async (req, res) => {
+  const { error } = updatePostschema.validate(req.body); // 본문 스키마 충족조건 판별
+  if (error) {
+    return res.status(400).json({ errorMessage: error.details[0].message });
+  }
   try {
     const { postId } = req.params;
-    const { title, content } = req.body;
+    const { title, content, imgsrc } = req.body;
     const post = await Posts.findOne({ where: { postId } });
 
     // post matching
@@ -58,14 +129,14 @@ const updatePost = async (req, res) => {
         .json({ errorMessage: "게시글을 찾을 수 없습니다." });
     }
 
-    if (!title || !!content) {
+    if (!title || !content) {
       return res
         .status(400)
         .json({ errorMessage: "내용이 존재하지 않습니다." });
     }
 
     // update title and content
-    const updatePost = await post.update({ title, content });
+    const updatePost = await post.update({ title, content, imgsrc });
     if (!updatePost) {
       return res
         .status(400)
@@ -80,7 +151,12 @@ const updatePost = async (req, res) => {
   }
 };
 
+
 const deletePost = async (req, res) => {
+  const { error } = postSchema.validate(req.body); // 본문 스키마 충족조건 판별
+  if (error) {
+    return res.status(400).json({ errorMessage: error.details[0].message });
+  }
   try {
     const { postId } = req.params;
     const post = await Posts.findOne({ where: { postId } });
