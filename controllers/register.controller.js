e91f6ff340401efcb6ec6255a4dc33837070a506
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-18 04:52:23 +0900

    test : 가나여?

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -53,7 +53,7 @@ const loginController = async (req, res) => {
     const token = jwt.sign({ name }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.cookie("userToken", `Bearer ${token}`);
+    res.cookie("userToken", `Bearer ${token}`, { path: "/" });
 
     return res
       .status(200)
