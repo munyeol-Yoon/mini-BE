9ac1fad1ffc57b9ab41c6ea9b811fac941b47a89
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-18 17:13:49 +0900

    fix : 이미지 validation 오류

--- validations/posts-validation.js
+++ validations/posts-validation.js
@@ -2,20 +2,21 @@ const Joi = require("joi");
 
 const postSchema = Joi.object({
   title: Joi.string().trim().empty("").max(50).required().messages({
+    "any.required": "제목을 입력해주세요.",
     "string.max": `'제목'은 {#limit} 글자 이하로 입력해주세요.`,
     "any.only": "형식이 올바르지 않습니다.",
     "any.invalid": "형식이 올바르지 않습니다.",
-    "any.required": "제목을 입력해주세요.",
   }),
   content: Joi.string().trim().empty("").max(300).required().messages({
+    "any.required": "내용을 입력해주세요.",
     "string.max": `'내용'은 {#limit} 글자 이하로 입력해주세요.`,
     "any.only": "형식이 올바르지 않습니다.",
     "any.invalid": "형식이 올바르지 않습니다.",
-    "any.required": "내용을 입력해주세요.",
   }),
-  imgsrc: Joi.string().uri().allow(null).optional(),
+  //   imgsrc: Joi.string().uri().allow(null).optional(),
 });
 
 module.exports = {
   postSchema,
+  updatePostschema,
 };
