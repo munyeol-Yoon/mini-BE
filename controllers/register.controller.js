fd4d5d3e3030d8da8d027730d14dc9fc3e815732
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-19 14:33:00 +0900

    update : cookie 옵션 추가

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -53,7 +53,7 @@ const loginController = async (req, res) => {
     const token = jwt.sign({ name }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.cookie("userToken", `Bearer ${token}`);
+    res.cookie("userToken", `Bearer ${token}`, { samesite: false });
 
     return res
       .status(200)
