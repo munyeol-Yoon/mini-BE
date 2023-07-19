504e6ead0c17a638b36c0ad3059b0aebed0d8be2
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-19 14:25:05 +0900

    update : cors option 변경

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 let corsOptions = {
-  origin: "*",
+  origin: "https://fe-mini-project.vercel.app/",
   credential: true,
   // optionsSuccessStatus: 200,
   // exposedHeaders: ["Authorization"],
